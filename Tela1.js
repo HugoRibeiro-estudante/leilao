464d5302babdfb6aa548383c2638a46c7a42b681
HugoRibeiro-estudante <hugo.alves@estudante.iftm.edu.br>
2023-10-10 21:52:51 -0300

    entregando atividade

--- Tela1.js
+++ Tela1.js
@@ -174,7 +174,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   finalizarLeilao: { 
-    fontSize: 16,
+    fontSize: 16, 
     color: 'blue',
     textDecorationLine: 'underline',
   },
